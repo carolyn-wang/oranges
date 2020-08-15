5707e310af7558ec7ec4e84d007ebfeb23f13b2f
carolyn-wang <69578355+carolyn-wang@users.noreply.github.com>
2020-08-15 04:08:50 -0700

    Delete passcode.js

--- passcode.js
+++
@@ -1,9 +0,0 @@
-module.exports = function generateNamespace(length = 6) {
-    var passcode = '';
-    var characters = 'ABCDEFGHIJKLMNPQRSTUVWXYZ123456789';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-       passcode += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return passcode;
- }
